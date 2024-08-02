8c139ea8ecb6c9b5403c1f2dabb69046d9a49d3c
xuchengshan <xuchengshan@hikvision.com.cn>
2024-08-02 14:59:46 +0800

    【spel】

---
+++ src/main/java/com/example/springbootdemo/spel/Main.java
@@ -0,0 +1,27 @@
+package com.example.springbootdemo.spel;
+
+import org.apache.dubbo.common.utils.Assert;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        Object user = new Object() {
+            public String getName() {
+                return "abc";
+            }
+        };
+        EvaluationContext context = new StandardEvaluationContext(user);
+        ExpressionParser parser = new SpelExpressionParser();
+        context.setVariable("hname", "1234");
+        String name = parser.parseExpression("#name").getValue(context, String.class);
+        String hname = parser.parseExpression("#hname").getValue(context, String.class);
+        String value = parser.parseExpression("getName()").getValue(context, String.class);
+
+        System.out.println("name:"+name);
+        System.out.println("hname:"+hname);
+        System.out.println("value:"+value);
+    }
+}
